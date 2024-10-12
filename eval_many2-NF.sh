fac3606f31b17a05a673f015b0685cbfaeedad79
kaichen-z <kc.zhou2020@hotmail.com>
2024-10-11 22:02:44 -0400

    ‘update12’

--- eval_many2-NF.sh
+++ eval_many2-NF.sh
@@ -1,5 +1,5 @@
 python -m manydepth2.evaluate_depth_many2 \
     --data_path /root/autodl-tmp/kai/data/1_mgdepth/KITTI  \
-    --load_weights_folder logs/weights/kitti_many2 \
+    --load_weights_folder logs/models_many2_nf/models/weights \
     --eval_mono \
     --using_flow 0
